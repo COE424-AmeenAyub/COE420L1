3345c6c036bd0c6eabd0c2c109930540cab5c2c8
yousuf <yousef.zahid@gmail.com>
2023-01-31 17:54:33 +0400

    Exercise 3 Commit

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
+package COE420L1;
+
 class Main {
+	
+	public static void featureB() {
+		System.out.println("B");
+	}
+	
   public static void main(String[] args) {
     System.out.println("Hello World");
+    featureB();
   }
 }
+
